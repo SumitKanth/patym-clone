921933266c53eda762d89fd8af3ce173c1f4a943
Sumit <sumitkanth999@gmail.com>
2022-07-17 21:52:48 +0530

    added sectionOne and sectionTwo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import SectionOne from "./Components/SectionOne";
+import SectionTwo from "./Components/SectionTwo";
 
 const App = () => {
   return(
     <>
     <Navbar />
+    <SectionOne />
+    <SectionTwo />
     </>
   )
 }
